8410f4cfd9a17337d4beec132710e81aec5e6405
Hassan Fouad <mrhassanfouad1@gmail.com>
2022-08-23 23:16:52 +0200

    Simple ws conenction

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 type job struct {
@@ -39,13 +41,58 @@ func createJob(c *gin.Context) {
 
 }
 
+var upgrader = websocket.Upgrader{
+	//check origin will check the cross region source (note : please not using in production)
+	CheckOrigin: func(r *http.Request) bool {
+
+		//Here we just allow the chrome extension client accessable (you should check this verify accourding your client source)
+		return true
+	},
+}
+
 func routes() {
 	router := gin.Default()
 	router.GET("/jobs", getJObs)
 	router.POST("/jobs", createJob, getJObs)
+
+	router.GET("/socket", func(c *gin.Context) {
+		//upgrade get request to websocket protocol
+		ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer ws.Close()
+		for {
+			//Read Message from client
+			mt, message, err := ws.ReadMessage()
+
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			//If client message is ping will return pong
+			if string(message) == "ping" {
+				message = []byte("pong")
+			}
+			//Response message to client
+			err = ws.WriteMessage(mt, message)
+			println("Sent " + string(message) + " to The Client")
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+		}
+	})
+
 	router.Run("localhost:8080")
+}
+
+func websocketServer(ws *websocket.Conn) {
+
 }
 
 func main() {
 	routes()
+
 }
